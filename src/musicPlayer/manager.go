4a60de62e1abaa2a1099e1791039c05da7529ccd
acer <acer@acer-PC>
2014-10-17 20:55:34 +0800

    ok2?

--- src/musicPlayer/manager.go
+++ src/musicPlayer/manager.go
@@ -17,7 +17,7 @@ type MusicManager struct {
 }
 
 func NewMusicManager() *MusicManager {
-	return &MusicManager{make([]MusicEntry, 0)}
+	return &MusicManager{make([]Music, 0)}
 }
 
 func (m *MusicManager) Len() int {
@@ -57,9 +57,23 @@ func (m *MusicManager) Remove(index int) *Music {
 	if index < len(m.musics)-1 {
 		m.musics = append(m.musics[:index-1], m.musics[index+1:]...)
 	} else if index == 0 {
-		m.musics = make([]MusicEntry, 0)
+		m.musics = make([]Music, 0)
 	} else {
 		m.musics = m.musics[:index-1]
 	}
 	return removedMusic
+}
+
+func (m *MusicManager) RemoveByName(name string) *Music {
+	if len(m.musics) == 0 {
+		return nil
+	}
+
+	for index, music := range m.musics {
+		if music.Name == name {
+			m.musics = append(m.musics[:index-1], m.musics[index+1:]...)
+			return &music
+		}
+	}
+	return nil
 }
