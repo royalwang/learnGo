a888d82e25e30602e4e726dc690a639560c4d731
acer <acer@acer-PC>
2014-10-21 10:27:54 +0800

    aga

--- src/test.go
+++ src/test.go
@@ -1,8 +1,18 @@
 package main
-import ("fmt"
+
+import (
+	"fmt"
 )
 
-func main(){
-    fmt.Println("Hello, world.  你好，世界！")
+func main() {
+	fmt.Println("Hello, world.  你好，世界！")
+	ch := make(chan int, 1)
+	for {
+		select {
+		case ch <- 0:
+		case ch <- 1:
+		}
+		i := <-ch
+		fmt.Println("Value received:", i)
+	}
 }
-
