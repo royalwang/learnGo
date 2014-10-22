13953d2c56ea3e95ca8f379274fc42bb252b3371
McGee <McGee@McGee-PC>
2014-10-22 10:14:44 +0800

    fgj

--- src/test.go
+++ src/test.go
@@ -4,15 +4,15 @@ import (
 	"fmt"
 )
 
-func main() {
-	fmt.Println("Hello, world.  你好，世界！")
-	ch := make(chan int, 1)
-	for {
-		select {
-		case ch <- 0:
-		case ch <- 1:
-		}
-		i := <-ch
-		fmt.Println("Value received:", i)
-	}
-}
+//func main() {
+//	fmt.Println("Hello, world.  你好，世界！")
+//	ch := make(chan int, 1)
+//	for {
+//		select {
+//		case ch <- 0:
+//		case ch <- 1:
+//		}
+//		i := <-ch
+//		fmt.Println("Value received:", i)
+//	}
+//}
